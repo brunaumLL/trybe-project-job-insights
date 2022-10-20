8c5307379b684548bd21d2e80b64f868c223e829
brunaumLL <bruno99louzada@gmail.com>
2022-10-20 16:56:27 -0300

    req 03 feito

--- src/insights.py
+++ src/insights.py
@@ -57,7 +57,13 @@ def get_unique_industries(path):
     list
         List of unique industries
     """
-    return []
+    jobs = read(path)
+    unique_industries = set()
+    for job in jobs:
+        if job['industry']:
+            industries = job['industry']
+            unique_industries.add(industries)
+    return unique_industries
 
 
 def filter_by_industry(jobs, industry):

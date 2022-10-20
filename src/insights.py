129c9144114126ad018960c1370c56f445082ba5
brunaumLL <bruno99louzada@gmail.com>
2022-10-20 16:34:21 -0300

    req 02 feito

--- src/insights.py
+++ src/insights.py
@@ -1,3 +1,6 @@
+from src.jobs import read
+
+
 def get_unique_job_types(path):
     """Checks all different job types and returns a list of them
 
@@ -13,7 +16,12 @@ def get_unique_job_types(path):
     list
         List of unique job types
     """
-    return []
+    jobs = read(path)
+    unique_jobs = set()
+    for job in jobs:
+        all_jobs = job['job_type']
+        unique_jobs.add(all_jobs)
+    return unique_jobs
 
 
 def filter_by_job_type(jobs, job_type):

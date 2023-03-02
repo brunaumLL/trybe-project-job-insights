dbe8ce75755fd3b663f9138453dc060d7b2791d6
brunaumLL <bruno99louzada@gmail.com>
2023-03-02 15:28:49 -0300

    requisito 08 feito

--- src/insights.py
+++ src/insights.py
@@ -33,12 +33,12 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    industry_list = list()
+    job_industry_list = list()
 
     for job in jobs:
         if job['industry'] == industry:
-            industry_list.append(job)
-    return industry_list
+            job_industry_list.append(job)
+    return job_industry_list
 
 
 def get_max_salary(path):
@@ -64,29 +64,19 @@ def get_min_salary(path):
 
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    if (
+      "min_salary" not in job.keys() or
+      "max_salary" not in job.keys() or
+      type(job["min_salary"]) != int or
+      type(job["max_salary"]) != int or
+      job["min_salary"] > job["max_salary"] or
+      type(salary) != int
+    ):
+        raise ValueError
+    if job["min_salary"] <= salary <= job["max_salary"]:
+        return True
+    else:
+        return False
 
 
 def filter_by_salary_range(jobs, salary):

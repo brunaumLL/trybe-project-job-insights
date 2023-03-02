2241e69053e5f326f36cc873c84f5f7bbb36e9ac
brunaumLL <bruno99louzada@gmail.com>
2023-03-02 17:22:43 -0300

    requisito 11 feito

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,15 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    path = 'tests/mocks/brazilians_jobs.csv'
+    dicts = read_brazilian_file(path)
+
+    for dict in dicts:
+        title = "title" in dict.keys()
+        salary = "salary" in dict.keys()
+        type = "type" in dict.keys()
+
+        assert title is True
+        assert salary is True
+        assert type is True

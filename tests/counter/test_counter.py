d5188f26be38481526304ad3e4a23eafe4fe3254
brunaumLL <bruno99louzada@gmail.com>
2023-03-02 17:03:39 -0300

    requisito 10 feito

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,7 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = 'src/jobs.csv'
+    assert count_ocurrences(path, 'Python') == 1639
+    assert count_ocurrences(path, 'Javascript') == 122
